98d9f042d24c9b1f6a2896e80bd8627f9fbf66fd
Gabriel Caiana <gabriel@server.signainfo.com.br>
2020-02-07 16:39:59 -0300

    create title and author dinamic in modal

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const closeModal = document.querySelector('.close')
 
 for(let receita of receitas) {  
     receita.addEventListener('click', function(){ 
-        let receitaId = receita.getAttribute('id')
-        console.log(receitaId)      
+        // const img = receita.querySelector('img').getAttribute('src')
+        // const title = receita.querySelector('p').innerText
+        // const author = receita.querySelector('span').innerText
+
+        const img = this.getAttribute('data-img')
+        const title = this.getAttribute('data-title')
+        const autor = this.getAttribute('data-author')
+
         modalOverlay.classList.add('active')
-        modal.querySelector("img").src = `${receitaId}`
+        modal.querySelector("img").src = img
+        modal.querySelector('.title-modal').innerText = title
+        modal.querySelector('.autor-modal').innerHTML = autor
     })
 }
 
