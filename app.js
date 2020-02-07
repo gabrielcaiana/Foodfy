671107f243dfa73b0856ba5010788ff1c0d27859
Gabriel Caiana <gabriel@server.signainfo.com.br>
2020-02-07 15:20:40 -0300

    modal images dinamic

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const modalOverlay = document.querySelector('.modal-overlay')
+const modal = document.querySelector('.modal')
 const receitas = document.querySelectorAll('.receita-item')
 const closeModal = document.querySelector('.close')
 
-for(receita of receitas) {  
-    receita.addEventListener('click', function(){
+for(let receita of receitas) {  
+    receita.addEventListener('click', function(){ 
+        let receitaId = receita.getAttribute('id')
+        console.log(receitaId)      
         modalOverlay.classList.add('active')
+        modal.querySelector("img").src = `${receitaId}`
     })
 }
 
