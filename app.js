c3f5c1ca8c4ca91f0c569b35850bc94301be4c8f
Gabriel Caiana <gabriel@server.signainfo.com.br>
2020-02-07 11:50:11 -0300

    create function close modal

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const receitas = document.querySelectorAll('.receita-item')
+const closeModal = document.querySelector('.close')
 
 for(receita of receitas) {  
     receita.addEventListener('click', function(){
         modalOverlay.classList.add('active')
     })
 }
+
+closeModal.addEventListener('click', function(){
+    modalOverlay.classList.toggle('active')
+})
