0f3ff4f828314cacd5fce645979ec141455a94c1
Gabriel Caiana <gabriel@server.signainfo.com.br>
2020-02-24 16:09:35 -0300

    update

--- public/app.js
+++ public/app.js
@@ -1,25 +1,26 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const modal = document.querySelector('.modal')
-const receitas = document.querySelectorAll('.receita-item')
-const closeModal = document.querySelector('.close')
+// const modalOverlay = document.querySelector('.modal-overlay')
+// const modal = document.querySelector('.modal')
+// const receitas = document.querySelectorAll('.receita-item')
+// const closeModal = document.querySelector('.close')
 
-for(let receita of receitas) {  
-    receita.addEventListener('click', function(){ 
-        // const img = receita.querySelector('img').getAttribute('src')
-        // const title = receita.querySelector('p').innerText
-        // const author = receita.querySelector('span').innerText
 
-        const img = this.getAttribute('data-img')
-        const title = this.getAttribute('data-title')
-        const autor = this.getAttribute('data-author')
+// for(let receita of receitas) {  
+//     receita.addEventListener('click', function(){ 
+//         // const img = receita.querySelector('img').getAttribute('src')
+//         // const title = receita.querySelector('p').innerText
+//         // const author = receita.querySelector('span').innerText
 
-        modalOverlay.classList.add('active')
-        modal.querySelector("img").src = img
-        modal.querySelector('.title-modal').innerText = title
-        modal.querySelector('.autor-modal').innerHTML = autor
-    })
-}
+//         const img = this.getAttribute('data-img')
+//         const title = this.getAttribute('data-title')
+//         const autor = this.getAttribute('data-author')
 
-closeModal.addEventListener('click', function(){
-    modalOverlay.classList.toggle('active')
-})
+//         modalOverlay.classList.add('active')
+//         modal.querySelector("img").src = img
+//         modal.querySelector('.title-modal').innerText = title
+//         modal.querySelector('.autor-modal').innerHTML = autor
+//     })
+// }
+
+// closeModal.addEventListener('click', function(){
+//     modalOverlay.classList.toggle('active')
+// })
