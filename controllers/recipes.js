536202c458e78bd8fc218d845cdca5ccf1577447
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-08-18 13:57:09 -0300

    fix: url edit recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,38 +1,43 @@
-const recipes = require('../data.json')
+const recipes = require("../data.json");
 
+//SITE
 exports.index = function (req, res) {
-    return res.render('pages/site/index', {recipes}.recipes)
-}
-
+  return res.render("pages/site/index", { recipes }.recipes);
+};
 exports.recipes = (req, res) => {
-        res.render('pages/site/recipes', {recipes}.recipes)
-}
+  res.render("pages/site/recipes", { recipes }.recipes);
+};
+exports.url_recipe = (req, res) => {
+  const recipeId = req.params.id;
 
-exports.url_recipe =  (req, res) => { 
-    const recipeId = req.params.id
-    
-    const myRecipe = recipes.recipes.find(function(myRecipe){
-        if(myRecipe.id == recipeId) {
-            return true
-        }
-    })
-    
-    if(!myRecipe) {
-        return res.render('404')
+  const myRecipe = recipes.recipes.find(function (myRecipe) {
+    if (myRecipe.id == recipeId) {
+      return true;
     }
-    
-    return res.render('pages/site/myRecipe', {myRecipe})
-}
+  });
+
+  if (!myRecipe) {
+    return res.render("404");
+  }
+
+  return res.render("pages/site/myRecipe", { myRecipe });
+};
 
 //ADMIN
 exports.adm_index = (req, res) => {
-    res.render('pages/admin/index',{recipes}.recipes)
-}
+  res.render("pages/admin/index", { recipes }.recipes);
+};
 
 exports.show = (req, res) => {
-    res.send('Página de editar')
-}
+  const recipeId = req.params.id;
 
-
-
-    
+  const myRecipe = recipes.recipes.find(function (myRecipe) {
+    if (myRecipe.id == recipeId) {
+      return true;
+    }
+});
+    if (!myRecipe) {
+      return res.render("404");
+    }
+    return res.render("pages/admin/show", { myRecipe });
+};
