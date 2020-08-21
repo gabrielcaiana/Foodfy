61c1bb861765af371488a2b7cd8318a01b65b00e
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-08-20 22:15:10 -0300

    add: inputs dinamics

--- public/app.js
+++ public/app.js
@@ -40,3 +40,46 @@ for (let i = 0; i < botaoEsconder.length; i++) {
         }  
     })
 }
+
+// document.querySelector(".btn").addEventListener("click", function(){
+//     alert('Hello DO APP')
+//     console.log('Hello')
+// })
+
+function addIngredient() {
+    const ingredients = document.querySelector("#ingredients");
+    const fieldContainer = document.querySelectorAll(".ingredient");
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[1].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[1].value = "";
+    ingredients.appendChild(newField);
+  }
+
+  function addPreparation() {
+    const preparation = document.querySelector("#preparation-mode");
+    const fieldContainer = document.querySelectorAll(".preparation");
+    
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[1].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[1].value = "";
+    preparation.appendChild(newField);
+  }
+  
+  document
+    .querySelector(".add-ingredient")
+    .addEventListener("click", addIngredient);
+
+    document
+    .querySelector(".add-preparation")
+    .addEventListener("click", addPreparation);
